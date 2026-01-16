a0833b252b330ded14631a2f3a4163df44f91fe1
vcatafesta <vcatafesta@gmail.com>
2026-01-16 13:23:23 -0400

    sex 16 jan 2026 13:23:23 -04 Vilmar Catafesta (vcatafesta@gmail.com)

---
+++ etc/profile.d/gtk-bookmarks.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+bk="$HOME/.config/gtk-3.0/bookmarks"
+
+# só continua se existir o arquivo
+[ -f "$bk" ] || exit 0
+
+# só faz a substituição se ainda contém placeholder
+grep -q "__HOME__" "$bk" || exit 0
+
+# substitui placeholder pelo $HOME real
+sed -i "s|__HOME__|$HOME|g" "$bk"
+
+# para todos os usuários
+for u in /home/*; do
+	[ -d "$u" ] || continue
+	bk="$u/.config/gtk-3.0/bookmarks"
+	[ -f "$bk" ] || continue
+	sed -i "s|__HOME__|$u|g" "$bk"
+done
